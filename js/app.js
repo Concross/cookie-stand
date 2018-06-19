c204ab27133f5d6a0063a185309e1fb840bc4414
Connor Crossley <cacrossley22@gmail.com>
2018-06-19 14:01:39 -0700

    Created new salmon store instances, added function to render all to the sales page

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function SalmonCookieStore(storeName, minHourlyCustomers, maxHourlyCustomers, av
   // Add this object instance to the stores Array
   salmonCookieStoresArray.push(this);
 }
-
+SalmonCookieStore.hoursOpenArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 // Method that returns a random number of customers within an objects min/max
 SalmonCookieStore.prototype.randomHourlyCustomers = function () {
   return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
@@ -49,7 +49,7 @@ SalmonCookieStore.prototype.render = function () {
   thEl.textContent = this.storeName;
   trEl.appendChild(thEl);
 
-  for (var hour in this.hoursOpenArray){
+  for (var hour in this.hoursOpenArray) {
     var tdEl = document.createElement('td');
     tdEl.textContent = this.hourlyCookiesArray[hour];
     trEl.appendChild(tdEl);
@@ -60,173 +60,39 @@ SalmonCookieStore.prototype.render = function () {
   salmonShopSectionEl.appendChild(trEl);
 };
 
-new SalmonCookieStore('College and Pence', 23, 65, 6.3, 15);
+var createHeaderRow = function () {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  trEl.appendChild(thEl);
+  console.log('hey!');
+  console.log(SalmonCookieStore.hoursOpenArray);
+  for (var hour in SalmonCookieStore.hoursOpenArray) {
+    console.log('in for loop');
+    thEl = document.createElement('th');
+    thEl.textContent = SalmonCookieStore.hoursOpenArray[hour];
+    trEl.appendChild(thEl);
+  }
+  thEl = document.createElement('th');
+  thEl.textContent = 'Daily Total';
+  trEl.appendChild(thEl);
 
-// salmonCookieStoresArray[0].simulatedHourlyCookies();
-// salmonCookieStoresArray[0].calcDailyCookiesTotal();
-salmonCookieStoresArray[0].render();
+  salmonShopSectionEl.appendChild(trEl);
+};
 
-// // College and Pence location object literal
-// var collegeAndPence = {
-//   storeName: 'College and Pence',
-//   minHourlyCustomers: 23,
-//   maxHourlyCustomers: 65,
-//   avgCookiesPerCustomer: 6.3,
-//   hourlyCookiesArray: [],
-//   dailyCookiesTotal: 0
-// };
-// // College and Pence randomHourlyCustomers method
-// collegeAndPence.randomHourlyCustomers = function () {
-//   return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-// };
-// // College and Pence simulatedHourlyCookies method
-// //Simulates the number of cookies sold per hour for this location
-// collegeAndPence.simulatedHourlyCookies = function () {
-//   var hoursOpen = 15;
-//   // Add a random number of customers times avg cookies per hour for each hour open
-//   for (var i = 0; i < hoursOpen; i++) {
-//     this.hourlyCookiesArray.push(Math.ceil(this.randomHourlyCustomers() * this.avgCookiesPerCustomer));
-//   }
-// };
-// // College and Pence render section
-// collegeAndPence.render = function () {
-//   this.simulatedHourlyCookies();
+new SalmonCookieStore('College and Pence', 23, 65, 6.3);
+new SalmonCookieStore('Chandler and 14th', 3, 24, 1.2);
+new SalmonCookieStore('Fresno and 14th', 11, 38, 3.7);
 
-//   // Calculate daily total cookies for this shop
-//   for (var cookies in this.hourlyCookiesArray) {
-//     this.dailyCookiesTotal += this.hourlyCookiesArray[cookies];
-//   }
-//   var h2El = document.createElement('h2');
-//   h2El.textContent = this.storeName;
+// Render header row and all current salmon cookie store data to the sales page
+var renderSalesTable = function () {
+  createHeaderRow();
 
-//   var ulEl = document.createElement('ul');
+  for(var store in salmonCookieStoresArray){
+    salmonCookieStoresArray[store].render();
+  }
+};
 
-//   // Create list items for each hour and render it
-//   for (var i in this.hourlyCookiesArray) {
-//     var liEl = document.createElement('li');
-//     if ((parseInt(i) + 6) < 12) { // Check if it's before 12pm
-//       liEl.textContent = (parseInt(i) + 6) + 'am: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     } else if (parseInt(i) === 6) { // If it is 12pm
-//       liEl.textContent = (parseInt(i) + 6) + 'pm: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     } else { // If its after 12 pm
-//       liEl.textContent = (parseInt(i) - 6) + 'pm: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     }
-//     ulEl.appendChild(liEl);
-//   }
-//   liEl = document.createElement('li');
-//   liEl.textContent = 'Total: ' + this.dailyCookiesTotal + ' cookies';
-//   ulEl.appendChild(liEl);
-
-//   salmonShopSectionEl.appendChild(h2El);
-//   salmonShopSectionEl.appendChild(ulEl);
-
-// };
-
-// // Chandler and 14th location object literal
-// var chandlerAnd14th = {
-//   storeName: 'Chandler and 14th',
-//   minHourlyCustomers: 3,
-//   maxHourlyCustomers: 24,
-//   avgCookiesPerCustomer: 1.2,
-//   hourlyCookiesArray: [],
-//   dailyCookiesTotal: 0
-// };
-// // Chandler and 14th randomHourlyCustomers method
-// chandlerAnd14th.randomHourlyCustomers = function () {
-//   return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-// };
-// // Chandler and 14th simulatedHourlyCookies method
-// //Simulates the number of cookies sold per hour for this location
-// chandlerAnd14th.simulatedHourlyCookies = function () {
-//   var hoursOpen = 15;
-//   for (var i = 0; i < hoursOpen; i++) {
-//     this.hourlyCookiesArray.push(Math.ceil(this.randomHourlyCustomers() * this.avgCookiesPerCustomer));
-//   }
-// };
-// // Chandler and 14th render section
-// chandlerAnd14th.render = function () {
-//   this.simulatedHourlyCookies();
-
-//   for (var cookies in this.hourlyCookiesArray) {
-//     this.dailyCookiesTotal += this.hourlyCookiesArray[cookies];
-//   }
-//   var h2El = document.createElement('h2');
-//   h2El.textContent = this.storeName;
-
-//   var ulEl = document.createElement('ul');
-
-//   for (var i in this.hourlyCookiesArray) {
-//     var liEl = document.createElement('li');
-//     if ((parseInt(i) + 6) < 12) {
-//       liEl.textContent = (parseInt(i) + 6) + 'am: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     } else if (parseInt(i) === 6) {
-//       liEl.textContent = (parseInt(i) + 6) + 'pm: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     } else {
-//       liEl.textContent = (parseInt(i) - 6) + 'pm: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     }
-//     ulEl.appendChild(liEl);
-//   };
-//   liEl = document.createElement('li');
-//   liEl.textContent = 'Total: ' + this.dailyCookiesTotal + ' cookies';
-//   ulEl.appendChild(liEl);
-
-//   salmonShopSectionEl.appendChild(h2El);
-//   salmonShopSectionEl.appendChild(ulEl);
-
-// };
-
-// // Fresno and 14th object literal
-// var fresnoAnd14th = {
-//   storeName: 'Fresno and 14th',
-//   minHourlyCustomers: 11,
-//   maxHourlyCustomers: 38,
-//   avgCookiesPerCustomer: 3.7,
-//   hourlyCookiesArray: [],
-//   dailyCookiesTotal: 0
-// };
-// // Fresno and 14th randomHourlyCustomers method
-// fresnoAnd14th.randomHourlyCustomers = function () {
-//   return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-// };
-// // Fresno and 14th simulatedHourlyCookies method
-// //Simulates the number of cookies sold per hour for this location
-// fresnoAnd14th.simulatedHourlyCookies = function () {
-//   var hoursOpen = 15;
-//   for (var i = 0; i < hoursOpen; i++) {
-//     this.hourlyCookiesArray.push(Math.ceil(this.randomHourlyCustomers() * this.avgCookiesPerCustomer));
-//   }
-// };
-// // Fresno and 14th render section
-// fresnoAnd14th.render = function () {
-//   this.simulatedHourlyCookies();
-
-//   for (var cookies in this.hourlyCookiesArray) {
-//     this.dailyCookiesTotal += this.hourlyCookiesArray[cookies];
-//   }
-//   var h2El = document.createElement('h2');
-//   h2El.textContent = this.storeName;
-
-//   var ulEl = document.createElement('ul');
-
-//   for (var i in this.hourlyCookiesArray) {
-//     var liEl = document.createElement('li');
-//     if ((parseInt(i) + 6) < 12) {
-//       liEl.textContent = (parseInt(i) + 6) + 'am: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     } else if (parseInt(i) === 6) {
-//       liEl.textContent = (parseInt(i) + 6) + 'pm: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     } else {
-//       liEl.textContent = (parseInt(i) - 6) + 'pm: ' + this.hourlyCookiesArray[i] + ' cookies';
-//     }
-//     ulEl.appendChild(liEl);
-//   }
-//   liEl = document.createElement('li');
-//   liEl.textContent = 'Total: ' + this.dailyCookiesTotal + ' cookies';
-//   ulEl.appendChild(liEl);
-
-//   salmonShopSectionEl.appendChild(h2El);
-//   salmonShopSectionEl.appendChild(ulEl);
-
-// };
+renderSalesTable();
 
 // // Minnesota and Bond object literal
 // var minnesotaAndBond = {
